051e75f489196f229cf49745bb428ceb131fa1b2
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2022-08-12 10:50:05 +0200

    updated vueapp

--- Demo/vueapp/src/views/LocationShow.vue
+++ Demo/vueapp/src/views/LocationShow.vue
@@ -1,41 +1,24 @@
 <template>
-  <section v-if="location" class="location">
+  <section class="location">
     <h1>{{ location.name }}</h1>
-    <dic class="location-details">
+    <dic class="locatio-details">
       <img :src="'/images/' + location.image" :alt="location.name" />
     </dic>
   </section>
 </template>
 <script>
-//import sourceData from "@/data.json";
+import sourceData from "@/data.json";
 
 export default {
-  data() {
-    return {
-      location: null,
-    };
-  },
   computed: {
     locationId() {
       return parseInt(this.$route.params.id);
     },
-  },
-  async created() {
-    const response = await fetch(
-      //"https://travel-dummy-api.netlify.app/${this.$route.params.slug}"
-      "https://travel-dummy-api.netlify.app/brazil"
-    );
-    this.location = await response.json();
-    this.$watch(
-      () => this.$route.params,
-      async () => {
-        const response = await fetch(
-          //"https://travel-dummy-api.netlify.app/${this.$route.params.slug}"
-          "https://travel-dummy-api.netlify.app/brazil"
-        );
-        this.location = await response.json();
-      }
-    );
+    location() {
+      return sourceData.locations.find(
+        (location) => location.id == this.locationId
+      );
+    },
   },
 };
 </script>
