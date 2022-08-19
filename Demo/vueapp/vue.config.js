e7ecbd93e18fe9d5ef6aa5fe58353b34f802d857
MKMIbrahim <49289724+mkmibrahim@users.noreply.github.com>
2022-08-19 13:22:18 +0200

    Modified Vue environment variables

--- Demo/vueapp/vue.config.js
+++ Demo/vueapp/vue.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
 });
 module.exports = {
   devServer: {
-    proxy: "http://localhost:5000"
+    // proxy: "http://localhost:5000"
+    proxy: process.env.BACKEND_API
     }
 }
