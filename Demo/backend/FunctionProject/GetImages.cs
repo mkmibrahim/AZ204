5301eff7d9e2f44afa194860a48f2a776e87c26d
MKMIbrahim <m.ibrahm2@krohne.com>
2022-09-01 16:49:12 +0200

    Update 20220901_2

--- Demo/backend/FunctionProject/GetImages.cs
+++ Demo/backend/FunctionProject/GetImages.cs
@@ -18,7 +18,7 @@ namespace FunctionProject
     {
         [FunctionName("GetImages")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
