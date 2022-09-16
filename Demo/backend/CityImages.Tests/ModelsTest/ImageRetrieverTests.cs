09babc9cb054ae876a7f253adb9ea7e37bc51f4d
Ibrahim <49289724+mkmibrahim@users.noreply.github.com>
2022-09-16 15:04:32 +0200

    Update 20220916

---
+++ Demo/backend/CityImages.Tests/ModelsTest/ImageRetrieverTests.cs
@@ -0,0 +1,38 @@
+﻿using CityImages.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CityImages.Tests.ModelsTest
+{
+    public class ImageRetrieverTests
+    {
+        [Fact]
+        public void CreateImageRetriever()
+        {
+            // Arrange
+
+            // Act
+            var imageRetriever = new ImageRetriever();
+
+            //Assert
+            Assert.NotNull(imageRetriever);
+        }
+
+        [Fact]
+        public async Task GetImageTestAsync()
+        {
+            //Arrange
+            var imageRetriever = new ImageRetriever();
+
+            //Act
+            var result = await imageRetriever.RetrieveImages("Amsterdam");
+
+            // Assert
+            Assert.False(String.IsNullOrEmpty(result.FirstOrDefault()));
+        }
+    }
+}
---
+++ Demo/backend/CityImages/Models/IImageRetriever.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CityImages.Models
+{
+    public interface IImageRetriever
+    {
+        public Task<List<string>> RetrieveImages(string locationname, int numberOfImages );
+    }
+}
