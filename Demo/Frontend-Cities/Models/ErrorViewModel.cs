f4e5b0f86cfc71afc18d90ad8794929b5b8f0a0f
mkmibrahim <m.ibrahim2@krohne.com>
2022-10-31 15:44:07 +0100

    Added Frontend-Cities

---
+++ Demo/Frontend-Cities/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Frontend_Cities.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
